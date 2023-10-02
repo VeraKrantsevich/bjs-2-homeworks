dc7823946ae5b6b3418551bd9671618a2047f62c
Vera Krantsevich <vera.krantsevich@gmail.com>
2023-10-02 16:12:26 +0300

    Задание к лекции 1 "Основые понятия" выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const d = Math.pow(b, 2) - 4 * a * c;
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  } else if (d === 0) {
+    arr[0] = -b / (2 * a);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthPercent =  percent / 100 / 12;
+  let loanBody = amount - contribution;
+  let monthPayment = loanBody * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+  let totalPayment = (monthPayment * countMonths).toFixed(2);
+  return Number(totalPayment);
 }
